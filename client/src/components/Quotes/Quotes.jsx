c7a71b4035ae6e19459a83767cca8caa2b628f56
Fiodor Gorobet <iamfiogo@gmail.com>
2022-12-14 11:51:57 +0100

    fix issue by quote destruction

--- client/src/components/Quotes/Quotes.jsx
+++ client/src/components/Quotes/Quotes.jsx
@@ -10,8 +10,6 @@ export default function Quotes() {
     setQuote(data)
   }
 
-  const { quote: text, author } = quote
-
   useEffect(() => {
     getRandomQuote()
   }, [])
@@ -23,8 +21,8 @@ export default function Quotes() {
           className="border rounded-lg w-full text-center text-sm p-2 mt-4 cursor-pointer select-none"
           // onClick={getRandomQuote}
         >
-          <div className="font-serif antialiased mb-1">{text}</div>
-          <div className="font-mono antialiased">- {author}</div>
+          <div className="font-serif antialiased mb-1">{quote?.quote}</div>
+          <div className="font-mono antialiased">- {quote?.author}</div>
         </div>
       )}
     </>
